a0e4b513a6249cbaf58398753933408f55e6f1ed
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-08 02:00:38 +0100

    adding stuff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - entry point of the program
+ *
+ *
+ * Description: prints the given string to standard output
+ *
+ * Return: returns 0 if succeeded
+ */
+
 int main()
 {
     puts("\"Programming is like building a multilingual puzzle");
