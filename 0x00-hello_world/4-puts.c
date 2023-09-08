80bceed1045e1053c3bc987d4812ae7513725ad8
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-08 02:17:48 +0100

    adding stuff

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,11 @@
 /**
  * main - entry point of the program
  *
- *
- * 
- *
  * Return: returns 0 if succeeded
  */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
