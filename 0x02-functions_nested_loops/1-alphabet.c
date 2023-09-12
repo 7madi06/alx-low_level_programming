21cd64967fa793d776c5c5f3b408dc296fe36a65
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-12 20:35:03 +0100

    adding stuff

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main -Entry point
+ * Description: 'prints the alphabet, in lowercase, followed by a new line.'
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+    print_alphabet();
+
+	return (0);
+}
