4051b750c4a227a68426b4018a50ae5e9b74cb3d
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-12 23:09:05 +0100

    adding stuff

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,13 @@
 
 void print_alphabet(void)
 {
-    for (int n = 97; n <= 122; n++)
+    int n;
+
+    for (n = 97; n <= 122; n++)
     {
         _putchar(n);
     }
+    _putchar("\n");
+
 	return (0);
 }
