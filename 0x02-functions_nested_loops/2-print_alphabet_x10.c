f5833816d2e89e6749d533c30d12a1f69439a036
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-13 01:16:51 +0100

    adding stuff

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,20 +2,20 @@
 
 /**
  * print_alphabet10x - function that prints the alphabet10x
- * Description: 'prints the alphabet, in lowercase, followed by a new line.10 times'
+ * Description: 'prints the alphabet, 10 times'
  * Return: Always 0 (Success)
 */
 
 void print_alphabet_x10(void)
 {
-    int i, n;
-   
-    for (i = 0; i < 10; i++)
+	int i, n;
+
+	for (i = 0; i < 10; i++)
 	{
-        for (n = 97; n < 123; n++)
-        {
-            _putchar(n);
-        }
-        _putchar('\n');
-    }
+		for (n = 97; n < 123; n++)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
 }
