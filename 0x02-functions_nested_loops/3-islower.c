e8915092642ce67cdb08a5d6121b0feab57367f4
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-13 02:17:17 +0100

    adding stuff

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,14 @@
 /**
  * _islower - function that prints the alphabet10x
  * Description: 'prints the alphabet, 10 times'
- * Return: 0 if input is lowercase or 1 if not (Success)
+ * Return: 1 if input is lowercase or 0 if not (Success)
 */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
-		return (0);
-	}else
-	{
-		return(1);
-	}
+		return (1);
+	else
+		return(0);
+
 }
