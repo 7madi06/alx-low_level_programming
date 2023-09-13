c56262cf3a99ffda119692f2e8fa388402c8f384
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-13 03:20:31 +0100

    adding stuff

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks if input is lowercase or uppercase
+ * Description: checking things
+ * @n: This is a character in ASCII code.
+ * Return: 1 if n > zero, 0 if n is zero, -1 if n < zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
