6a7b7cac4d0663ae1c06ee434c96a75a54658e1a
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-18 21:31:12 +0100

    adding stuff

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,10 +4,10 @@
 /**
  * _strlen - returns the length of a string.
  * @s : int
- * Return : the length of a string
+ * Return: returns the length of a string
 */
 
 int _strlen(char *s)
 {
-	return(strlen(s));
+	return (strlen(s));
 }
