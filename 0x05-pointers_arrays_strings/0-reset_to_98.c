7c1b7822d6243133b40579139433a72089b69e01
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-18 20:57:05 +0100

    adding stuff

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int
  *				and updates the value it points to > 98
- * Description: 'changes the value of intput to 98'
+ * @n : int pointing to input
 */
 
 void reset_to_98(int *n)
