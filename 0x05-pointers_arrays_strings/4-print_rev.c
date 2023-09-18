a3bb64f6bf4f8306a7d39a0f742f2b996960e340
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-18 23:19:12 +0100

    adding stuff

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
 * print_rev - prints a string, in reverse, followed by a new line.
-* @s : int
+* @s : char
 */
 
 void print_rev(char *s)
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string.
+* @s : char
+*/
+
+void rev_string(char *s)
+{
+
+	int i, j, count = 0;
+
+	while (s[count] != '\0')
+		count++;
+
+	j = count - 1;
+	char tempstr[count];
+
+	for (i = 0; i < count; i++)
+	{
+		tempstr[i] = s[j];
+		j--;
+	}
+}
