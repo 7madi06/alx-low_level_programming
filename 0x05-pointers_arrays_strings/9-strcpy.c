b5424e34a9d2b1158a23ade8b644a56a38c07b22
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-19 02:02:57 +0100

    adding stuff

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _strcpy - prints n elements of an array of integers.
+* @dest : char
+* @src : char
+* Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+
+}
