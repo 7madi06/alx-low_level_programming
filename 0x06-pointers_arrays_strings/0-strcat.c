08d986305452d9f06b70a9a0ee975d153586a7e6
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-20 13:36:27 +0100

    adding stuff

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,5 +9,24 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int i, j, dest_len = 0, src_len = 0;
 
+	while (dest[dest_len] != '\0')
+		dest_len++;
+	while (src[src_len] != '\0')
+		src_len++;
+	int final_len = dest_len + src_len;
+
+	i = dest_len;
+
+	for (i = dest_len; i < final_len;)
+	{
+		for (j = 0; j < src_len; j++)
+		{
+			dest[i] = src[j];
+			i++;
+		}
+	}
+	dest[final_len] = '\0';
+	return (dest);
 }
