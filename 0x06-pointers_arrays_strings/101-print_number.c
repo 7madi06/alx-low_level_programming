5839d37ab500695dc8a21b8882dc40f853fd05c6
7madi_06 <youssefsaddouki4@gmail.com>
2023-09-21 22:04:48 +0100

    adding stuff

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,10 +2,9 @@
 
 /**
  * print_number - prints an integer.
- * @n: int.
+ * @n: input integer.
  * Return: no return.
  */
-
 void print_number(int n)
 {
 	unsigned int m, d, count;
@@ -19,13 +18,16 @@ void print_number(int n)
 	{
 		m = n;
 	}
-	d = m, count = 1;
+
+	d = m;
+	count = 1;
 
 	while (d > 9)
 	{
 		d /= 10;
 		count *= 10;
 	}
+
 	for (; count >= 1; count /= 10)
 	{
 		_putchar(((m / count) % 10) + 48);
