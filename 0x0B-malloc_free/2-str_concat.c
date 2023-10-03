95e80513de05a0c67a5f0d6198ac4c3b6dccd7b1
7madi_06 <youssefsaddouki4@gmail.com>
2023-10-03 22:57:27 +0100

    adding stuff

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,50 +10,34 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *concatenated;
-	int len_s1 = 0, len_s2 = 0, total_len = 0;
-	int i, j;
+	char *strout;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	while (s1[len_s1] != '\0')
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = malloc(sizeof(char) * (i + j + 1));
+
+	if (strout == NULL)
 	{
-		len_s1++;
-	}
-
-	while (s2[len_s2] != '\0')
-	{
-		len_s2++;
-	}
-
-	total_len = len_s1 + len_s2;
-
-	concatenated = (char *)malloc(total_len + 1);
-
-	if (concatenated == NULL)
-	{
+		free(strout);
 		return (NULL);
 	}
 
-	for (i = 0; i < len_s1; i++)
-	{
-		concatenated[i] = s1[i];
-	}
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-	for (j = 0; j < len_s2; j++)
-	{
-		concatenated[i + j] = s2[j];
-	}
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
-	concatenated[total_len] = '\0';
-
-	return (concatenated);
+	return (strout);
 }
