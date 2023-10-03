0491a0d99c01add30e2e5ba373f9c0c61ccdd8b5
7madi_06 <youssefsaddouki4@gmail.com>
2023-10-04 00:52:50 +0100

    adding stuff

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,43 +4,28 @@
  * main - entrypoint.
  * Return: 1.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-	if (argc != 2)
+	int cents, coins[] = {25, 10, 5, 2, 1};
+	int total_coins = 0;
+
+	printf("Enter the amount of cents: ");
+	if (scanf("%d", &cents) != 1)
 	{
-		printf("Error\n");
+		fprintf(stderr, "Error: Invalid input\n");
 		return (1);
 	}
 
-	int cents = atoi(argv[1]);
-
-	if (cents < 0)
+	if (cents >= 0)
 	{
-		printf("0\n");
-		return (0);
+		for (int i = 0; i < 5; i++)
+		{
+			total_coins += cents / coins[i];
+			cents %= coins[i];
+		}
 	}
-
-	int coins25 = cents / 25;
-
-	cents %= 25;
-	int coins10 = cents / 10;
-
-	cents %= 10;
-	int coins5 = cents / 5;
-
-	cents %= 5;
-	int coins2 = cents / 2;
-
-	cents %= 2;
-	int coins1 = cents;
-
-	int total_coins = coins25 + coins10 + coins5 + coins2 + coins1;
 
 	printf("%d\n", total_coins);
 
 	return (0);
 }
-
-
-
-
